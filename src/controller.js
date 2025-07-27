7a8548db28dc5b86e37e4aea9e6f38a344331c2e
Lorenzo Pachioli <lorenzopachioli@gmail.com>
2025-07-27 16:27:49 -0300

    codigo limpiado y batchs procesados al mismo tiempo

--- src/controller.js
+++ src/controller.js
@@ -1,66 +1,23 @@
 const Records = require('./records.model');
-const { vaciarCarpetaTemp } = require('./utils');
-const fs = require('fs');
-const csv = require('csv-parser');
+const { vaciarCarpeta, splitCsvByLines, formatRecords, arrayOfPathFn } = require('./utils');
 
 const upload = async (req, res) => {
     const {file} = req;
-
-    if (!file) {
-        return res.status(400).json({ message: 'No file uploaded' });
-    }
-
     const batchSize = 90000;
-    let batch = [];
-    let header = 0;
+    
+    if (!file)  return res.status(400).json({ message: 'No file uploaded' });
 
-    try {
-        const stream = fs.createReadStream(file.path)
-            .pipe(csv({ headers: true }));
+    try{
+        const numFiles = await splitCsvByLines(file.path, batchSize, './_temp');
+        const arrayOfPromises = arrayOfPathFn(numFiles, formatRecords);
 
-        stream.on('data', async (data) => {
-            if( header == 0) { // Si es la primera fila, es el header del csv y lo ignoro
-                header++;
-            } else {
-                // Les doy el formato del modelo a cada fila del csv
-                const formatted = {
-                    id: parseInt(data._0, 10) ? parseInt(data._0, 10) : 0,
-                    firstname: data._1,
-                    lastname: data._2,
-                    email: data._3,
-                    email2: data._4,
-                    profession: data._5
-                };
+        await Promise.all(arrayOfPromises);
+        await vaciarCarpeta('./_temp');
 
-                // Agrego el objeto formateado al arreglo batch
-                batch.push(formatted);
-
-                // Si el batch alcanza el tamaño definido, lo inserto en la base de datos
-                if (batch.length >= batchSize) {
-                    stream.pause();
-                    await Records.insertMany(batch);
-                    batch = [];
-                    stream.resume();
-                }
-            }
-        });
-
-        header = 0;
-        stream.on('end', async () => {
-            if (batch.length > 0) {
-                await Records.insertMany(batch);
-            } 
-            await vaciarCarpetaTemp('./_temp');            // Elimino el archivo temporal
-            return res.status(200).json({ message: 'Archivo subido exitosamente', data: file });
-        });
-
-        stream.on('error', (err) => {
-            return res.status(500).json({ message: 'Error al procesar archivo', error: err });
-        });
-
-
+        return res.status(200).json({ message: 'Archivo subido exitosamente', data: [] });
     } catch (err) {
-        return res.status(500).json(err);
+        await vaciarCarpeta('./_temp'); 
+        return res.status(500).json({ message: 'Error al procesar el archivo', error: err });
     }
 };
 
--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,136 @@
-const fs = require('fs').promises;
-const path = require('path');
+const fs = require("node:fs");
+const fsPromise = require("node:fs").promises;
+const path = require("node:path");
+const csv = require("csv-parser");
+const Records = require("./records.model");
 
-const vaciarCarpetaTemp = async (directorio) => {
-    const archivos = await fs.readdir(directorio);
-    for (const archivo of archivos) {
-        const ruta = path.join(directorio, archivo);
-        await fs.unlink(ruta);
-    }
+const vaciarCarpeta = async (directorio) => {
+  const archivos = await fsPromise.readdir(directorio); //crea un array con los nombres de los archivos en el directorio
+  
+  for (const archivo of archivos) { 
+    const ruta = path.join(directorio, archivo);
+    await fsPromise.unlink(ruta); // Elimina cada archivo en el directorio
+  }
+};
+
+async function splitCsvByLines(filePath, batchSize, tempDir) {
+  return new Promise((resolve, reject) => {
+
+    if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir, { recursive: true });
+
+    const readStream = fs.createReadStream(filePath, { encoding: "utf-8" });
+    let fileIndex = 0;
+    let lineCount = 0;
+    let writeStream;
+    let leftover = "";
+    let header = null;
+    let isFirstLine = true;
+
+    readStream.on("data", (chunk) => {
+      let lines = (leftover + chunk).split("\n");
+      leftover = lines.pop(); // Guarda la última línea para el siguiente chunk
+
+      for (let line of lines) {
+
+        if (isFirstLine) {
+          header = line;
+          // Primer archivo, escribe encabezado
+          writeStream = fs.createWriteStream(path.join(tempDir, `part_${fileIndex}.csv`));
+          writeStream.write(header + "\n");
+          isFirstLine = false;
+          continue;
+        }
+
+        if (lineCount === 0 && fileIndex > 0) {
+          // Nuevo archivo, escribe encabezado
+          writeStream = fs.createWriteStream(path.join(tempDir, `part_${fileIndex}.csv`));
+          writeStream.write(header + "\n");
+        }
+
+        // Agrega la línea al archivo actual
+        writeStream.write(line + "\n");
+        lineCount++;
+
+        if (lineCount >= batchSize) {
+          // Cierra el archivo actual y comienza uno nuevo
+          writeStream.end();
+          fileIndex++;
+          lineCount = 0;
+        }
+      }
+    });
+
+    readStream.on("end", async () => {
+
+      if (leftover) writeStream.write(leftover + "\n"); // Si hay una línea sobrante, la escribe
+      writeStream.end();
+      await fsPromise.unlink(filePath); // Elimina el archivo original luego de procesarlo
+      resolve(fileIndex + 1); // Número de archivos generados
+    });
+
+    readStream.on("error", reject);
+  });
 }
 
+const formatRecords = async (path) => {
+
+  let batch = [];
+  let header = 0;
+
+  return new Promise((resolve, reject) => { // creo una promesa para manejar el flujo de datos del csv de manera asincrona
+    try {
+      const stream = fs.createReadStream(path).pipe(csv({ headers: true }));
+
+      stream.on("data", async (data) => {
+        if (header == 0) {
+          // Si es la primera fila, es el header del csv y lo ignoro
+          header++;
+        } else {
+          // Les doy el formato del modelo a cada fila del csv
+          const formatted = new Records({
+            id: parseInt(data._0, 10) ? parseInt(data._0, 10) : 0,
+            firstname: data._1,
+            lastname: data._2,
+            email: data._3,
+            email2: data._4,
+            profession: data._5,
+          });
+
+          // Agrego el objeto formateado al arreglo batch
+          batch.push(formatted);
+        }
+      });
+
+      header = 0;
+      stream.on("end", async () => {
+        await Records.insertMany(batch); 
+        batch = []; // Limpio el batch para liberar memoria
+        return resolve(batch);
+      });
+
+      stream.on("error", (err) => {
+        return reject({ message: "Error al procesar archivo", error: err });
+      });
+    } catch (err) {
+      return reject(err);
+    }
+  });
+};
+
+const arrayOfPathFn = (numFiles, fn) => {
+  const functions = [];
+
+  // Genera un array de funciones que toman la ruta del archivo como argumento
+  for (let i = 0; i < numFiles; i++) {
+    const filePath = `./_temp/part_${i}.csv`;
+    functions.push(fn(filePath));
+  }
+  return functions;
+};
+
 module.exports = {
-    vaciarCarpetaTemp
+  vaciarCarpeta,
+  splitCsvByLines,
+  arrayOfPathFn,
+  formatRecords,
 };
